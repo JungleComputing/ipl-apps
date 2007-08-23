074cff606e7197d1ed3b85871de7a8a8db487e1c
Niels Drost <n.drost@esciencecenter.nl>
2007-08-23 16:37:29 +0000

    added capability to generate events to the registry benchmark application

--- src/ibis/ipl/apps/benchmarks/registry/Main.java
+++ src/ibis/ipl/apps/benchmarks/registry/Main.java
@@ -1,74 +1,73 @@
 package ibis.ipl.apps.benchmarks.registry;
 
-
 import ibis.util.Log;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-
 public final class Main {
- 
-    private static final Logger logger = Logger.getLogger(Main.class);
-    
-    private final Application[] apps;
-    
-    Main(int threads, boolean sync, int step) {
-        
-        apps = new Application[threads];
-        for (int i = 0; i < threads ; i++) {
-            logger.debug("starting thread " + i + " of " + threads);
-            try {
-                apps[i] = new Application(i == (threads - 1), step);
-                if (sync) {
-                    //wait for the ibis to be initialized
-                    apps[i].run();
-                } else {
-                    //fork of a thread for the application to run in
-                    apps[i].start();
-                }
-            } catch (Exception e) {
-                e.printStackTrace(System.err);
-            }
-        }
-    }
-    
-    public static void main(String[] args) {
-        int threads = 1;
-        int step = 1;
-        boolean sync = false;
 
-	Log.initLog4J("ibis.ipl.apps", Level.INFO);
-        
-        for (int i = 0; i < args.length;i++) {
-            if (args[i].equalsIgnoreCase("--threads")) {
-                i++;
-                threads = new Integer(args[i]);
-            } else if (args[i].equalsIgnoreCase("--sync")) {
-                sync = true;
-            } else if (args[i].equalsIgnoreCase("--step")) {
-                i++;
-                step = new Integer(args[i]);
-            } else {
-                System.err.println("unknown option: " + args[i]);
-                System.exit(1);
-            }
-        }
-            
-        new Main(threads, sync, step);
+	private static final Logger logger = Logger.getLogger(Main.class);
 
-      
-        
-        while(true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                //IGNORE
-            }
-        }
-    }
-    
-   
+	private final IbisApplication[] apps;
 
+	private final boolean generateEvents;
+	private final boolean generateLeaves;
+
+	Main(int threads, boolean generateEvents, boolean generateLeaves) {
+		this.generateEvents = generateEvents;
+		this.generateLeaves = generateLeaves;
+
+		apps = new IbisApplication[threads];
+		for (int i = 0; i < threads; i++) {
+			logger.debug("starting thread " + i + " of " + threads);
+			apps[i] = new IbisApplication(generateEvents, generateLeaves);
+		}
+	}
+
+	void printStats() {
+		int totalSeen = 0;
+		for (int i = 0; i < apps.length; i++) {
+			totalSeen += apps[i].nrOfIbisses();
+		}
+		double average = (double) totalSeen / (double) apps.length;
+
+		System.out.printf("average seen members = %.2f  (total = %d)\n",
+				average, apps.length);
+	}
+
+	public static void main(String[] args) {
+		int threads = 1;
+		boolean generateEvents = false;
+		boolean generateLeaves = false;
+
+		Log.initLog4J("ibis.ipl.apps", Level.INFO);
+
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equalsIgnoreCase("--threads")) {
+				i++;
+				threads = new Integer(args[i]);
+			} else if (args[i].equalsIgnoreCase("--events")) {
+				generateEvents = true;
+			} else if (args[i].equalsIgnoreCase("--leaves")) {
+				generateEvents = true;
+				generateLeaves = true;
+			} else {
+				System.err.println("unknown option: " + args[i]);
+				System.exit(1);
+			}
+		}
+
+		Main main = new Main(threads, generateEvents, generateLeaves);
+
+		while (true) {
+			try {
+				main.printStats();
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// IGNORE
+			}
+		}
+	}
 
 }
